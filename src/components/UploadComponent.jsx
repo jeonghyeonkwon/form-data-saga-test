e6bd40dbf1e8a59d1118c3a4da93565b75b76d23
jeonghyeonkwon <givejeong2468@gmail.com>
2023-03-21 22:56:01 +0900

    업로드 폼 배치중

--- src/components/UploadComponent.jsx
+++ src/components/UploadComponent.jsx
@@ -3,15 +3,32 @@ import styled from "styled-components";
 
 const UploadForm = styled.div`
   width: 90%;
-  height: 100px;
+  height: 300px;
   background-color: dodgerblue;
-  margin: 20px 0px;
+
+  display: flex;
+  overflow: hidden;
+
+  border-radius: 10px;
 `;
-const TitleForm = styled.div``;
+const TitleForm = styled.div`
+  height: 100%;
+  width: 100%;
+
+  background-color: gray;
+`;
+const DeleteForm = styled.div`
+  height: 100%;
+  background-color: crimson;
+  width: 120px;
+  padding: 20px;
+  color: white;
+`;
 function UploadComponent({ data, onClickDelete }) {
   return (
     <UploadForm>
       <TitleForm>{data.title}</TitleForm>
+      <DeleteForm>삭제하기</DeleteForm>
     </UploadForm>
   );
 }
