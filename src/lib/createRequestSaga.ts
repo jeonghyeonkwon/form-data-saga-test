2853df9cd02c7e53b93669f073469e56fc0e2fbb
jeonghyeonkwon <givejeong2468@gmail.com>
2023-05-01 16:19:05 +0900

    createRequestSaga ts  전환

--- src/lib/createRequestSaga.ts
+++ src/lib/createRequestSaga.ts
@@ -2,25 +2,36 @@ import { call, put } from "redux-saga/effects";
 
 import { startLoading, finishLoading } from "../modules/loading";
 
-export const createRequestActionTypes = (type) => {
+export const createRequestActionTypes = (type: string) => {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
   return [type, SUCCESS, FAILURE];
 };
 
-export default function createRequestSaga(type, request) {
+export interface ResponseGenerator {
+  config?: any;
+  data?: any;
+  headers?: any;
+  request?: any;
+  status?: number;
+  statusText?: string;
+}
+export default function createRequestSaga(type: string, request: any) {
   const SUCCESS = `${type}_SUCCESS`;
   const FAILURE = `${type}_FAILURE`;
 
-  return function* (action) {
+  return function* (action: any) {
     yield put(startLoading(type));
 
     try {
-      const response = yield call(request, action.payload);
+      const response: ResponseGenerator = yield call(
+        request,
+        action.payload as any
+      );
       console.log(response);
       yield put({
         type: SUCCESS,
-        payload: response.data,
+        payload: response.data as any,
         meta: response,
       });
     } catch (e) {
